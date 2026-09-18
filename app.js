eacc30461e2da3e4b3e3b9d65e8c231bb50a5803
yaz10 <webdevyaz10@gmail.com>
2026-09-18 15:39:09 +0300

    run the app and listen

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("it's running continuously again");
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty'
+    }
+  }
+});
+
+fastify.get("/", (req, res) => {
+  res.send({ hello: "world" });
+});
+
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  
+  // Server is now listening on ${address}
+});
